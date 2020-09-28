9eaf4fcca6ed54a650a97f624a51c435ded82c2b
mcarpenter-ucvts <43757511+mcarpenter-ucvts@users.noreply.github.com>
2020-09-27 21:35:48 -0400

    Update Exercise8.java
    done

--- src/Exercise8.java
+++ src/Exercise8.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Exercise8 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Students : ");
+        int studentsTotal= input.nextInt();
+
+        System.out.print("Teachers : ");
+        int teachersTotal= input.nextInt();
+
+        System.out.print("Capacity : ");
+        int busCapacity = input.nextInt();
+
+        int busNum = (studentsTotal + teachersTotal + busCapacity -1) / busCapacity;
+        int busOverflow = (studentsTotal + teachersTotal)% busCapacity;
+
+        System.out.printf("\n");
+        System.out.print("Buses required      : " + busNum);
+        System.out.print("\nOverflow passengers : " + busOverflow);
+    }
 }

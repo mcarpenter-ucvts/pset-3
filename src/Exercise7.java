c3e05e3aa3d143e9a56905337e5f79fc0c7c5cf6
mcarpenter-ucvts <43757511+mcarpenter-ucvts@users.noreply.github.com>
2020-09-27 21:34:45 -0400

    Update Exercise7.java
    done

--- src/Exercise7.java
+++ src/Exercise7.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Exercise7 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Salary  : ");
+        double sal = input.nextDouble();
+
+        System.out.print("401(k)  : ");
+        double k = input.nextDouble();
+
+        System.out.print("Federal : ");
+        double fed = input.nextDouble();
+
+        System.out.print("State   : ");
+        double state = input.nextDouble();
+
+        double finalPay = sal * (1-k/100) * (1-(fed +state)/100) /24;
+        System.out.print("\n$");
+        System.out.printf("%,.2f", finalPay);
+        System.out.println(".");
+    }
+
 }
